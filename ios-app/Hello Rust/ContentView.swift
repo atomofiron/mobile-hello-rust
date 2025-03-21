ac8c6d54046a5480f23463f118cd8bba5a30205c
Curtes Malteser <antonio_bastiao@hotmail.com>
2025-03-21 23:22:57 +0100

    feat: Link Rust XCFramework with Swift project
    - Integrated hello_rust_lib.xcframework into Xcode
    - Enabled Swift to call Rust functions via FFI

--- ios-app/Hello Rust/ContentView.swift
+++ ios-app/Hello Rust/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    var hello: String {
+        guard let cString = reverse_greet("dlroW tsuR") else { return "Error calling Rust" }
+        defer { free_rust_string(UnsafeMutablePointer(mutating: cString)) }  // Ensure memory is freed after use
+        return String(cString: cString) // Convert to Swift String
+    }
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(hello)
+            Text("Sum from Rust: \(add(3, 2))")
+            Text("Subtraction from Rust: \(subtract(3, 2))")
         }
         .padding()
     }
